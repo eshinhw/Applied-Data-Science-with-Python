e16f25f3ed84a439ad4b0d17234e2abd0b1304bc
eshin <ehwshin@gmail.com>
2019-08-05 18:19:59 -0400

    Python Basics

---
+++ Week1.py
@@ -0,0 +1,21 @@
+# Python Dictionary
+
+contact = {'Bill': 'Bill@gmail.com', 'Kevin': 'Kevin@gmail.com'}
+
+for name in contact.keys():
+    print(name)
+
+for email in contact.values():
+    print(email)
+
+for name, email in contact.items():
+    print(name, email)
+
+
+x = ('Bill', 'Gates', 'gates@gmail.com')
+fname, lname, email = x
+
+print(fname, lname, email)
+
+# Python Date and Time
+
